95eb99bd205e953045ac5445f7087a6ebcc4bc50
beaumanvienna (JC) <beaumanvienna@gmail.com>
2020-08-30 08:39:21 -0700

    GTK3 for Linux Mint 20

--- pcsx2/detect_gtk_flags.sh
+++ pcsx2/detect_gtk_flags.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+
+#Ubuntu 20 and derivatives such as Linux Mint 20 need GTK3
+
 PRETTY_NAME=`grep PRETTY_NAME /etc/os-release`
 IS_UBUNTU=`echo $PRETTY_NAME | grep Ubuntu`
 IS_ARCH=`echo $PRETTY_NAME | grep Arch`
+IS_MINT=`echo $PRETTY_NAME | grep Mint`
 IS_VERSION_NUMBER_18_04=`echo $PRETTY_NAME | grep 18.04`
 IS_VERSION_NUMBER_20_04=`echo $PRETTY_NAME | grep 20.04`
+IS_VERSION_NUMBER_20=`echo $PRETTY_NAME | grep 20`
 
 if test -n "$IS_ARCH" 
 then
     #echo "Archibald found"
     DUMMY=`echo "null"`
+fi
+
+if test -n "$IS_MINT" 
+then
+
+    if test -n "$IS_VERSION_NUMBER_20" 
+    then
+          #echo "Version 20.04 found"
+          echo "-DGTK3_API=ON"
+    fi
 fi
 
 if test -n "$IS_UBUNTU" 
