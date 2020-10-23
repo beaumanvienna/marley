77ba384ca38307412e449a04bf16de50eed68cbb
beaumanvienna (JC) <beaumanvienna@gmail.com>
2020-10-22 20:23:21 -0700

    PCSX2: GTK detection revised

--- pcsx2/detect_gtk_flags.sh
+++ pcsx2/detect_gtk_flags.sh
@@ -3,7 +3,7 @@
 
 #Ubuntu 20 and derivatives such as Linux Mint 20 need GTK3
 
-PCSX2_USE_GTK3=`find /usr/ -name "*wx_gtk3u_core-3.0*.so"`
+PCSX2_USE_GTK3=`find /usr/ -name "*wx_gtk3u_core-3.0*.so" 2>/dev/null`
 
 if [ -z "$PCSX2_USE_GTK3" ]
 then
