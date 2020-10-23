e9ff550f89b05ff28cc6e70e0fb49dd4f5c126c9
beaumanvienna (JC) <beaumanvienna@gmail.com>
2020-10-22 20:02:27 -0700

    PCSX2: GTK detection revised

--- pcsx2/detect_gtk_flags.sh
+++ pcsx2/detect_gtk_flags.sh
@@ -3,51 +3,11 @@
 
 #Ubuntu 20 and derivatives such as Linux Mint 20 need GTK3
 
-PRETTY_NAME=`grep PRETTY_NAME /etc/os-release`
-IS_UBUNTU=`echo $PRETTY_NAME | grep Ubuntu`
-IS_ARCH=`echo $PRETTY_NAME | grep Arch`
-IS_MINT=`echo $PRETTY_NAME | grep Mint`
-IS_GENTOO=`uname -r | grep gentoo`
-IS_VERSION_NUMBER_18_04=`echo $PRETTY_NAME | grep 18.04`
-IS_VERSION_NUMBER_20_04=`echo $PRETTY_NAME | grep 20.04`
-IS_VERSION_NUMBER_20=`echo $PRETTY_NAME | grep 20`
+PCSX2_USE_GTK3=`find /usr/ -name "*wx_gtk3u_core-3.0*.so"`
 
-if test -n "$IS_ARCH" 
+if [ -z "$PCSX2_USE_GTK3" ]
 then
-    #echo "Archibald found"
     DUMMY=`echo "null"`
+else
+    echo "-DGTK3_API=ON"
 fi
-
-if test -n "$IS_GENTOO" 
-then
-    #echo "Gentoo found"
-    echo "-DGTK3_API=TRUE_GENTOO"
-fi
-
-if test -n "$IS_MINT" 
-then
-
-    if test -n "$IS_VERSION_NUMBER_20" 
-    then
-          #echo "Version 20.04 found"
-          echo "-DGTK3_API=ON"
-    fi
-fi
-
-if test -n "$IS_UBUNTU" 
-then
-    #echo "Ubuntu found"
-    if test -n "$IS_VERSION_NUMBER_18_04" 
-    then
-        #echo "Version 18.04 found"
-        DUMMY=`echo "null"`
-    fi
-
-    if test -n "$IS_VERSION_NUMBER_20_04" 
-    then
-          #echo "Version 20.04 found"
-          echo "-DGTK3_API=ON"
-    fi
-fi
-
-
